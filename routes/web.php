e96a2dcd278b1d807bb79f5b3a4a6c025377426e
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2024-11-22 16:28:17 +0700

    CODE: Starts working on admin features

--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,6 @@ Auth::routes();
 Route::get('/report', [App\Http\Controllers\ReportController::class, 'index'])->name('reports.index');
 Route::get('/report/photo/{id}', [App\Http\Controllers\ReportController::class, 'showPhoto'])->name('reports.photo.show');
 Route::get('/report/track/{token}', [App\Http\Controllers\ReportController::class, 'show'])->name('reports.share');
-
 Route::post('/report/store', [App\Http\Controllers\ReportController::class, 'store'])->name('reports.store');
+
+Route::get('/admin/dashboard', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.dashboard');
