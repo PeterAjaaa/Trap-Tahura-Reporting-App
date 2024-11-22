b2c4f682debd0ff0b540a2a7d6b619e965319790
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2024-11-22 14:35:29 +0700

    CODE: Start adding support to shareable link

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -3,9 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Report extends Model
 {
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($report) {
+            $report->shareable_token = Str::uuid();
+        });
+    }
+
     protected $fillable = [
         'title',
         'type',
--- database/migrations/2024_11_13_214327_create_reports_table.php
+++ database/migrations/2024_11_13_214327_create_reports_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('description');
             $table->string('status')->default('pending');
             $table->string('photo');
+            $table->string('shareable_token')->unique()->nullable();
             $table->unsignedBigInteger('admin_id')->nullable();
             $table->foreign('admin_id')->references('id')->on('users')->onDelete('set null');
             $table->timestamps();
