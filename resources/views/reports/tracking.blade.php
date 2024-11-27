1011e076c1241e34630798b9e6f0f0a58fc53f5e
PeterAjaaa <89441024+PeterAjaaa@users.noreply.github.com>
2024-11-27 22:48:32 +0700

    CODE: Add user close report feature

--- resources/views/reports/tracking.blade.php
+++ resources/views/reports/tracking.blade.php
@@ -29,42 +29,42 @@
     </script>
     <div class="container">
         <div class="row">
-            <div class="col-md-12 d-flex justify-content-start mb-3">
+            <div class="col-12 d-flex justify-content-start mb-3">
                 <h1 class="text-center">Laporan ID: {{ $report->id }} </h1>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6 d-flex justify-content-start mb-3">
+            <div class="col-6 d-flex justify-content-start mb-3">
                 <strong>
                     <p class="text-center">Laporan: {{ $report->title }}</p>
                 </strong>
             </div>
-            <div class="col-md-6 d-flex justify-content-start mb-3">
+            <div class="col-6 d-flex justify-content-start mb-3">
                 <strong>
                     <p class="text-center">Jenis Laporan: {{ $report->type }}</p>
                 </strong>
             </div>
-            <div class="col-md-6 d-flex justify-content-start mb-3">
+            <div class="col-6 d-flex justify-content-start mb-3">
                 <strong>
                     <p class="text-center">Deskripsi Laporan: {{ $report->description }}</p>
                 </strong>
             </div>
-            <div class="col-md-6 d-flex justify-content-start mb-3">
+            <div class="col-6 d-flex justify-content-start mb-3">
                 <strong>
                     <p class="text-center">Status Laporan: {{ $report->status }}</p>
                 </strong>
             </div>
-            <div class="col-md-12 d-flex justify-content-start mb-3">
+            <div class="col-6 d-flex justify-content-start mb-3">
                 <strong>
                     <p class="text-center">Admin Laporan: {{ $report->assignedAdmin->name }}</p>
                 </strong>
             </div>
-            <div class="col-md-12 d-flex justify-content-center mb-3">
+            <div class="col-12 d-flex justify-content-center mb-3">
                 <strong>
                     <p class="text-center">Foto Laporan:</p>
                 </strong>
             </div>
-            <div class="col-md-12 d-flex justify-content-center mb-3">
+            <div class="col-12 d-flex justify-content-center mb-3">
                 @if ($report->photo)
                     <img src="{{ route('reports.photo.show', $report->id) }}" alt="Foto Laporan"
                         class="img-fluid img-thumbnail vw-25">
@@ -76,8 +76,24 @@
                     </strong>
                 @endif
             </div>
-            <div class="col-md-12 d-flex justify-content-center mb-3">
+            <div class="col-12 d-flex justify-content-center mb-3">
                 <button class="btn btn-success" onclick="copyCurrentURL()">Copy Link</button>
+            </div>
+            <div style="height: 150px;"></div>
+            <div class="col-12 d-flex justify-content-center mb-3">
+                <strong>
+                    <p class="text-center text-danger">
+                        Jika Anda ingin menutup laporan lebih awal, klik tombol "Close Report"
+                    </p>
+                </strong>
+            </div>
+            <div class="col-12 d-flex justify-content-center mb-3">
+                <form action="{{ route('reports.update.status', $report->id) }}" method="POST">
+                    @csrf
+                    @method('PATCH')
+                    <input type="hidden" name="status" value="Closed">
+                    <button type="submit" class="btn btn-danger btn-sm">Close Report</button>
+                </form>
             </div>
         </div>
     </div>
